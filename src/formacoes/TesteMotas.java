3b1dba87e637c0f5fcd53780d5b0bc8d5a324794
Tropa <Tropa@Tropa.lan>
2016-01-19 21:22:05 +0000

    interface veiculos classe veiculos - mota main - testemotas

---
+++ src/formacoes/TesteMotas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formacoes;
+
+/**
+ *
+ * @author Tropa
+ */
+public class TesteMotas {
+
+    public static void Main (String[]args){
+    
+        Mota mota125=new Mota(125, false, '0');
+        Mota mota500=new Mota(500, true, '1');
+    
+        
+    }
+}
