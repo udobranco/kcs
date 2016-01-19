a2ed75d1f5ec0a3074088346189354b71ea2c66c
Tropa <Tropa@Tropa.lan>
2016-01-19 21:31:08 +0000

    formação do dia 19/01/2016

--- src/formacoes/Veiculos.java
+++ src/formacoes/Veiculos.java
@@ -7,13 +7,30 @@ package formacoes;
 
 /**
  *
- * @author Tropa
+ * @author Udo Branco <udobranco@gmail.com>
  */
 public abstract class  Veiculos implements IVeiculos{
     
+    /**
+     * nRodas - define o numero de rodas de um veiculo
+     */
     protected int nRodas;
+    
+    /**
+     * cilindrada - define a cilindrada de um veiculo
+     */
     protected int cilindrada;
+    
+    /**
+     * abs - define se um veiculo tem ou nao abs 
+     */
     protected boolean abs;
+    
+    /**
+     * startUpType - define o tipo de arranque do veiculo
+     *              0 ignicao normal
+     *              1 start and stop
+     */
     protected char startUpType;
     
     
