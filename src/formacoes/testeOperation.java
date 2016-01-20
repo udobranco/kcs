a31edb0909d20846eb6f39703240a9a21aa4a9ca
Tropa <Tropa@Tropa.lan>
2016-01-20 18:36:12 +0000

    formação do dia 20/01/2016

---
+++ src/formacoes/testeOperation.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formacoes;
+
+/**
+ *
+ * @author Udo Branco <udobranco@gmail.com>
+ */
+public class testeOperation {
+
+    public static void main(String[] args) {
+
+        Operation op = new Operation();
+
+        int v1 = 10, v2 = 5, v3 = 1;
+
+        System.out.println("Testing sum2val: " + op.sum2val(v1, v2));
+        System.out.println("Testing sub: " + op.sub(v1, v2));
+        System.out.println("Testing mul: " + op.mult(v1, v2));
+        System.out.println("Testing sum3val: " + op.sum3val(v1, v2, v3));
+    }
+
+}
