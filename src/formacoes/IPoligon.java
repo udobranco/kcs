2a161957e4f33f4e57b29429190dd9083aa3c8d9
Tropa <Tropa@Tropa.lan>
2016-01-20 22:56:14 +0000

    formação do dia 20/01/2016

---
+++ src/formacoes/IPoligon.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formacoes;
+
+/**
+ *
+ * @author Udo Branco <udobranco@gmail.com>
+ */
+public interface IPoligon {
+    
+    public abstract double getPerimetro();
+    public abstract Ponto[] getPontos();
+    
+}
